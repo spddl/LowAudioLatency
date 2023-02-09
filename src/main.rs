5cf15dac05116b03eb3716ca9cd19b707df4716d
spddl <spddlhh@gmail.com>
2023-02-09 23:03:53 +0100

    Now the Mic is tested as well

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // #![windows_subsystem = "windows"]
+
 use windows_dll::dll;
 
 enum CVoid {}
@@ -8,17 +9,36 @@ extern "system" {
     fn GetCurrentProcess() -> *mut CVoid;
 
     #[allow(non_snake_case)]
-    fn SetPriorityClass(
-        hProcess: *mut CVoid,
-        dwPriorityClass: u32,
-    ) -> i32;
+    fn SetPriorityClass(hProcess: *mut CVoid, dwPriorityClass: u32) -> i32;
 }
 mod sound;
 
 #[cfg(windows)]
 fn main() {
     // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-setpriorityclass
-    unsafe { SetPriorityClass(GetCurrentProcess(), 0x00100000); } // PROCESS_MODE_BACKGROUND_BEGIN
+    unsafe {
+        SetPriorityClass(GetCurrentProcess(), 0x00100000); // PROCESS_MODE_BACKGROUND_BEGIN
+    }
 
-    sound::apply_audio_settings();
+    let mut handles = Vec::new();
+    for data in [
+        (
+            windows::Win32::Media::Audio::eRender,
+            windows::Win32::Media::Audio::eConsole,
+        ),
+        (
+            windows::Win32::Media::Audio::eCapture,
+            windows::Win32::Media::Audio::eCommunications,
+        ),
+    ]
+    .iter()
+    {
+        handles.push(std::thread::spawn(move || {
+            sound::apply_audio_settings(data.0, data.1)
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
--- src/sound.rs
+++ src/sound.rs
@@ -1,26 +1,46 @@
 use windows::core::GUID;
 use windows::Win32::Media::Audio::{
-    eConsole, eRender, IAudioClient3, IMMDeviceEnumerator, MMDeviceEnumerator,
+    EDataFlow, ERole, IAudioClient3, IMMDeviceEnumerator, MMDeviceEnumerator,
 };
-use windows::Win32::System::Com::{CoCreateInstance, CoInitialize, CLSCTX_ALL};
+use windows::Win32::System::Com::{
+    CoCreateInstance, CoInitialize, StructuredStorage, CLSCTX_ALL, STGM_READ, VT_LPWSTR,
+};
+use windows::Win32::UI::Shell::PropertiesSystem::{IPropertyStore, PROPERTYKEY};
 
-pub fn apply_audio_settings() {
+#[allow(non_upper_case_globals)]
+const PKEY_Device_FriendlyName: PROPERTYKEY = PROPERTYKEY {
+    fmtid: GUID::from_values(
+        0xa45c254e,
+        0xdf1c,
+        0x4efd,
+        [0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0],
+    ),
+    pid: 14,
+};
+
+pub fn apply_audio_settings(dataflow: EDataFlow, role: ERole) {
     unsafe {
         CoInitialize(None).expect("CoInitialize Failed");
 
-        let imm_device_enumerator: IMMDeviceEnumerator =
+        let device_enumerator: IMMDeviceEnumerator =
             CoCreateInstance(&MMDeviceEnumerator, None, CLSCTX_ALL)
                 .expect("CoCreateInstance Failed");
 
-        let endpoint = imm_device_enumerator
-            .GetDefaultAudioEndpoint(eRender, eConsole)
+        let endpoint = device_enumerator
+            .GetDefaultAudioEndpoint(dataflow, role)
             .expect("GetDefaultAudioEnpoint Failed");
+
+        let property_store = endpoint
+            .OpenPropertyStore(STGM_READ)
+            .expect("OpenPropertyStore Failed");
 
         let p_audio_client: IAudioClient3 = endpoint
             .Activate(CLSCTX_ALL, None)
             .expect("Activate Failed");
 
         let p_format = p_audio_client.GetMixFormat().unwrap();
+
+        let friendly_name = get_property_vt_lpwstr(&property_store, &PKEY_Device_FriendlyName);
 
         let n_channels = (*p_format).nChannels;
         let w_bits_per_sample = (*p_format).wBitsPerSample;
@@ -42,6 +62,7 @@ pub fn apply_audio_settings() {
             )
             .expect("GetSharedModeEnginePeriod Failed");
 
+        println!("Friendly Name...........{}", friendly_name);
         println!("Channels{:.>17}", n_channels);
         println!("Bits per sample{:.>11} Bit", w_bits_per_sample);
         println!("Samples per sec{:.>11} kHz", n_samples_per_sec / 1000);
@@ -83,4 +104,43 @@ pub fn apply_audio_settings() {
 
         std::thread::park();
     }
+}
+
+fn get_property_vt_lpwstr(store: &IPropertyStore, props_key: &PROPERTYKEY) -> String {
+    #[allow(unused_assignments)]
+    let mut result = String::from("");
+    unsafe {
+        let mut property_value = store.GetValue(props_key as *const _ as *const _).unwrap();
+
+        let prop_variant = &property_value.Anonymous.Anonymous;
+
+        if prop_variant.vt != VT_LPWSTR {
+            println!(
+                "property store produced invalid data: {:?}",
+                prop_variant.vt
+            );
+        }
+
+        let ptr_utf16 = *(&prop_variant.Anonymous as *const _ as *const *const u16);
+
+        // Find the length of the friendly name.
+        let mut len = 0;
+        while *ptr_utf16.offset(len) != 0 {
+            len += 1;
+        }
+
+        // Create the utf16 slice and convert it into a string.
+        let name_slice = std::slice::from_raw_parts(ptr_utf16, len as usize);
+        let name_os_string: std::ffi::OsString =
+            std::os::windows::ffi::OsStringExt::from_wide(name_slice);
+        let name_string = match name_os_string.into_string() {
+            Ok(string) => string,
+            Err(os_string) => os_string.to_string_lossy().into(),
+        };
+
+        StructuredStorage::PropVariantClear(&mut property_value).ok();
+
+        result = name_string
+    }
+    result
 }

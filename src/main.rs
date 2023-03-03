0e7d40b35b79201d91a636d26aea3636d8709862
spddl <spddlhh@gmail.com>
2023-03-03 18:06:59 +0100

    remove "windows-dll"

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,14 @@
 // #![windows_subsystem = "windows"]
 
-use windows_dll::dll;
-
-enum CVoid {}
-#[dll("kernel32.dll")]
-extern "system" {
-    #[allow(non_snake_case)]
-    fn GetCurrentProcess() -> *mut CVoid;
-
-    #[allow(non_snake_case)]
-    fn SetPriorityClass(hProcess: *mut CVoid, dwPriorityClass: u32) -> i32;
-}
+use windows::Win32::System::Threading::*;
 mod sound;
 
 #[cfg(windows)]
 fn main() {
     // https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-setpriorityclass
-    unsafe {
-        SetPriorityClass(GetCurrentProcess(), 0x00100000); // PROCESS_MODE_BACKGROUND_BEGIN
-    }
+	unsafe {
+		SetPriorityClass(GetCurrentProcess(), PROCESS_MODE_BACKGROUND_BEGIN);
+	}
 
     let mut handles = Vec::new();
     for data in [

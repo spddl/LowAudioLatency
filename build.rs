90cd056bdac8ff95fb29cac9a8757d7124d1e45c
spddl <spddlhh@gmail.com>
2024-10-30 22:26:54 +0100

    Added icon and manifest for admin rights

---
+++ build.rs
@@ -0,0 +1,37 @@
+#[cfg(target_os = "windows")]
+use winres;
+
+#[cfg(target_os = "windows")]
+fn main() {
+    use std::io::Write;
+    // only build the resource for release builds
+    // as calling rc.exe might be slow
+    if std::env::var("PROFILE").unwrap() == "release" {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico").set_manifest(
+            r#"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+        <requestedPrivileges>
+            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+        </requestedPrivileges>
+    </security>
+</trustInfo>
+</assembly>
+"#,
+        );
+
+        match res.compile() {
+            Err(error) => {
+                write!(std::io::stderr(), "{}", error).unwrap();
+                std::process::exit(1);
+            }
+            Ok(_) => {}
+        }
+    }
+}
+
+#[cfg(not(target_os = "windows"))]
+fn main() {}

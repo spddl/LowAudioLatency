6bc8718b88133ede690fa437a129777dd9962031
spddl <spddlhh@gmail.com>
2024-11-12 11:14:56 +0100

    remove manifest

--- build.rs
+++ build.rs
@@ -8,20 +8,7 @@ fn main() {
     // as calling rc.exe might be slow
     if std::env::var("PROFILE").unwrap() == "release" {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("icon.ico").set_manifest(
-            r#"
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
-<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-<trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-    <security>
-        <requestedPrivileges>
-            <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-        </requestedPrivileges>
-    </security>
-</trustInfo>
-</assembly>
-"#,
-        );
+        res.set_icon("icon.ico");
 
         match res.compile() {
             Err(error) => {
